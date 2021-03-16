4c83eeedeb1f5d2a5069a94a7d0d799d59e8ec35
lcm97 <linchunmin1997@gmail.com>
2021-03-16 22:56:16 +0800

    indexpage

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,7 +10,7 @@
         <!--右上角按钮-->
         <div class="buttons">
             <div v-bind:class="[isplay?'on':'off']" v-on:click="playmusic">
-                <audio loop="loop" src="../static/SeeYouAgain.mp3" id="music" preload="auto"></audio>
+                <audio loop="loop" src="http://localhost:3000/upload/1615022619274n.mp3" id="music" preload="auto"></audio>
             </div> <!--音乐播放按钮 默认关闭-->
             <div class="button"><span>活动<br>玩法</span></div>
             <div class="button"><span>联系<br>机构</span></div>
@@ -78,6 +78,77 @@
              </div>
         </div>
 
+        <!--活动详情面板-->
+        <div class="detail_board">活动详情</div>
+        
+        <!--课程秒杀-->
+        <div class="course_board">
+            <div class="search_group">
+                <div style="font-size:15px;color:black;margin-right:5px">课程秒杀</div>
+            </div>
+            <!--课程项目-->
+            <div class="course_items" v-for="item in course_list" :key="item.id">
+                <div class="course_item">
+                    <van-image
+                    width="9rem"
+                    height="7rem"
+                    fit="cover"
+                    lazy-load
+                    :src="item.img"
+                    />
+                    <div class="middle_info2">
+                        <div class="course_name">{{item.name}}</div>
+                        <div class="course_class">{{item.class}}</div>
+                        <div class="prices">
+                            <span class="price_board">现价</span>
+                            <span class="price">¥{{item.price}}</span>
+                            <span class="origin_price">¥{{item.origin_price}}</span>
+                        </div>
+                        <div class="button_bottom">立即参团</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--蓝色分割条-->
+        <div class="blue_line"></div>
+        
+        <!--福利列表-->
+        <div class="welfare_list" v-for="(item,index) in welfare_list" :key="index">
+            <div class="welfare_item">
+                <div class="search_group">
+                    <div style="font-size:15px;color:black;margin-right:5px">{{item.name}}</div>
+                </div>
+                <div class="welfare_detail">
+                    <div style="margin:8px">{{item.detail}}</div>
+                </div>
+                <div class="welfare_imgs" v-for="(img,idx) in item.imgs" :key="idx">
+                    <van-image style="width:94%;margin-bottom:10px" fit="cover" lazy-load :src="img" />
+                </div>
+
+            </div>
+        </div>
+        <!--蓝色分割条-->
+        <div style="height: 24px;background-color: #4699f5;margin-left: 10px;margin-right:10px;margin-bottom:10px"></div>
+        <div class="group_board">
+            <div class="search_group">
+                <div style="font-size:18px;color:black;margin-right:5px">机构介绍</div>
+            </div>
+
+            <div class="company_list" v-for="(item,index) in company_list" :key="index">
+                <span style="margin:10px;font-size:15px;text-align: left;">{{item.describe}}</span>
+                <div class="welfare_imgs" v-for="(img,idx) in item.imglist" :key="idx">
+                    <van-image style="width:94%;margin-bottom:10px" fit="cover" lazy-load :src="img" />
+                </div>
+                <div class="contact_board">联系方式</div>
+                <div class="contact_phone">电话：{{item.phone}}</div>
+                <div class="contact_phone">地址：{{item.address}}</div>
+                <div class="welfare_imgs" v-for="(img,idx) in item.contacts" :key="idx">
+                    <van-image style="width:94%;margin-bottom:10px" fit="cover" lazy-load :src="img" />
+                </div>
+            </div>
+
+        </div>
         <!--立即开团按钮-->
         <div class="open_group"></div>
         <div class="open_group_button">立即开团</div>
@@ -146,14 +217,80 @@ export default {
           id: 791526,
           type: 1, // 组团人数类型
           num: 1 // 参团人数
-        },
-        {
-          img: 'https://img01.yzcdn.cn/vant/cat.jpeg',
-          name: '钟梓建',
-          id: 791527,
-          type: 1, // 组团人数类型
-          num: 1 // 参团人数
         }
+      ],
+      course_list:[{
+          id:1,
+          img: "https://img.yzcdn.cn/vant/cat.jpeg",
+          name:"功夫明星跆拳道",
+          price: 168.00,
+          origin_price: 200.00,
+          class:"6次12节"
+          },
+          {
+          id:2,
+          img: "https://img.yzcdn.cn/vant/cat.jpeg",
+          name:"功夫明星跆拳道",
+          price: 168.00,
+          origin_price: 200.00,
+          class:"6次12节"
+          },
+          {
+          id:3,
+          img: "https://img.yzcdn.cn/vant/cat.jpeg",
+          name:"功夫明星跆拳道",
+          price: 168.00,
+          origin_price: 200.00,
+          class:"6次12节"
+          }
+      ],
+      welfare_list:[
+          {
+              name:"福利标题",
+              detail: "福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字",
+              imgs:[
+                  "https://img.yzcdn.cn/vant/cat.jpeg",
+                  "https://img.yzcdn.cn/vant/cat.jpeg"
+              ]
+          },
+          {
+              name:"福利标题",
+              detail: "福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字福利描述文字",
+              imgs:[
+                  "https://img.yzcdn.cn/vant/cat.jpeg",
+                  "https://img.yzcdn.cn/vant/cat.jpeg"
+              ]
+          }
+      ],
+      company_list:[
+          {
+              describe:'机构介绍机构介绍文字机构介绍文字机构介绍文字机构介绍文字机构介绍文字机构介绍文字',
+              imglist:[
+                  'https://img.yzcdn.cn/vant/cat.jpeg',
+                  'https://img.yzcdn.cn/vant/cat.jpeg'
+              ],
+              phone:13424354657,
+              address: '上海市普陀区金沙江路 1518 弄',
+              contacts:[
+                  'https://img.yzcdn.cn/vant/cat.jpeg',
+                  'https://img.yzcdn.cn/vant/cat.jpeg'                  
+              ]
+
+          },
+          {
+              describe:'机构介绍机构介绍文字机构介绍文字机构介绍文字机构介绍文字机构介绍文字机构介绍文字',
+              imglist:[
+                  'https://img.yzcdn.cn/vant/cat.jpeg',
+                  'https://img.yzcdn.cn/vant/cat.jpeg'
+              ],
+              phone:13424354657,
+              address: '上海市普陀区金沙江路 1518 弄',
+              contacts:[
+                  'https://img.yzcdn.cn/vant/cat.jpeg',
+                  'https://img.yzcdn.cn/vant/cat.jpeg'                  
+              ]
+
+          }
       ]
 
     }
@@ -355,6 +492,154 @@ export default {
       background-color: #4699f5;
       z-index: 6;
   }
+  .detail_board{
+      width: 100%;
+      height: 42px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      color: white;
+      font-size: 18px;
+      text-align: center;
+      line-height: 42px;
+      background-color: #4699f5;
+  }
+  .course_board{
+      margin-right: 10px;
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+  }
+  .course_items{
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+  }
+  .course_item{
+      width: 100%;
+      height: 130px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      background-color: #feda02;
+      border-bottom: black dashed 1px;    
+  }
+
+  .middle_info2{
+      width: 53%;
+      height: 7rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: flex-start;
+  }
+  .course_name{
+      font-size: 15px;
+
+  }
+  .course_class{
+      font-size: 11px;
+      color: gray;
+  }
+  .prices{
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: flex-end;
+  }
+  .price_board{
+      width: 2rem;
+      height: 1rem;
+      background-color: red;
+      font-size: 13px;
+      color: whitesmoke;
+      border-radius: 15%;
+  }
+  .price{
+      font-size: 16px;
+      margin-left: 3px;
+  }
+  .origin_price{
+      font-size: 8px;
+      color: gray;
+      margin-left: 5px;
+      text-decoration: line-through;
+  }
+  .button_bottom{
+      width: 98%;
+      height: 25px;
+      background-color: white;
+      border: 1px black solid;
+      border-radius: 12%;
+
+  }
+  .blue_line{
+      height: 24px;
+      background-color: #4699f5;
+      margin: 10px;
+  }
+  .welfare_list{
+      margin-right: 10px;
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+  }
+  .welfare_item{
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      margin-bottom: 10px;
+  }
+  .welfare_detail{
+      width: 100%;
+      font-size: 15px;
+      text-align: left;
+      background-color: #feda02;
+      padding-bottom: 5px;
+      padding-top: 8px;
+  }
+  .welfare_imgs{
+      width: 100%;
+      background-color: #feda02;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+  }
+  .company_list{
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      background-color: #feda02;
+      border: black dashed 1px;
+      margin-bottom: 15px;
+
+  }
+  .contact_board{
+      width: 95%;
+      height: 30px;
+      background-color: whitesmoke;
+      line-height: 30px;
+  }
+  .contact_phone{
+      width: 95%;
+      text-align: left;
+      margin: 5px;
+  }
+
+
+
+
   .wave{
       position: fixed;
       bottom: 0;
